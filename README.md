ce3083812d1171c5055f7eda13e8434f18c1dd93
easydatawarehousing <info@easydatawarehousing.com>
2013-12-03 09:25:36 +0100

    Update 0.2.0

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-prestoclient
+PrestoClient
 ============
 
 PrestoClient implements a Python class to communicate with a Presto server.
@@ -8,7 +8,7 @@ Hadoop HDFS servers (http://hadoop.apache.org/).
 Presto uses SQL as its query language. Presto is an alternative for
 Hadoop-Hive.
 
-PrestoClient was developed using Presto 0.52 and tested on Presto 0.52 and 0.54.
+PrestoClient was developed using Presto 0.52 and tested on Presto 0.52 and 0.54. Python version used is 2.7.6
 
 You can use this class with this sample code:
 
@@ -20,43 +20,52 @@ You can use this class with this sample code:
 	presto = prestoclient.PrestoClient("localhost")
 	
 	if not presto.startquery(sql):
-	    print presto.getlasterrormessage()
+		print "Error: ", presto.getlasterrormessage()
 	else:
-	    presto.waituntilfinished(True) # Remove True parameter to skip printing status messages
-	    print "Columns: ", presto.getcolumns()
-	    print "Datalength: ", presto.getdatalength(), " Data: ", presto.getdata()
+		presto.waituntilfinished(True) # Remove True parameter to skip printing status messages
+		
+		# We're done now, so let's show the results
+		print "Columns: ", presto.getcolumns()
+		if presto.getstatus() == "FAILED": print "Error : ", presto.getlasterrormessage()
+		if presto.getdata(): print "Datalength: ", presto.getnumberofdatarows(), " Data: ", presto.getdata()
 
 
 Presto client protocol
 ----------------------
-
-The communication protocol used between Presto clients and servers is not documented. It seems to
+The communication protocol used between Presto clients and servers is not documented yet. It seems to
 be as follows:
 
-Client sends http POST request. Headerinformation should include: X-Presto-Catalog, X-Presto-Source,
-X-Presto-Schema, User-Agent, X-Presto-User. The body of the request should contain the sql statement.
-The server responds by returning JSON data. This data should contain 2 uri's. One giving the link
-to get more information about the query execution (infoUri) and the other one giving the link to fetch
-the next packet of data (nextUri).
+Client sends http POST request to the Presto server, page: "/v1/statement". Header information should
+include: X-Presto-Catalog, X-Presto-Source, X-Presto-Schema, User-Agent, X-Presto-User. The body of the
+request should contain the sql statement. The server responds by returning JSON data (http status-code 200).
+This reply may contain up to 3 uri's. One giving the link to get more information about the query execution
+('infoUri'), another giving the link to fetch the next packet of data ('nextUri') and one with the uri to
+cancel the query ('partialCancelUri').
 
-The client should send GET requests to the server (header: X-Presto-Source, User-Agent, X-Presto-User.
-Body: empty) following the nextUri link from the last response
-until the server response does not give any more nextUri links. The server response also contains a
-'state' variable. When there is no nextUri the state should be one of: FINISHED, FAILED or CANCELED.
-Each response by the server to a 'nextUri' may contain information about the columns returned by the
-query and all- or part of the querydata.
+The client should send GET requests to the server (Header: X-Presto-Source, User-Agent, X-Presto-User.
+Body: empty) following the 'nextUri' link from the previous response until the servers response does not
+contain an 'nextUri' link anymore. When there is no 'nextUri' the query is finished. If the last response
+from the server included an error section ('error') the query failed, otherwise the query succeeded. If
+the http status of the server response is anything other than 200 with Content-Type application/json, the
+query should also be considered failed. A 503 http response means that the server is (too) busy. Retry the
+request after waiting at least 50ms.
+The server response may contain a 'state' variable. This is for informational purposes only (may be subject
+to change in future implementations).
+Each response by the server to a 'nextUri' may contain information about the columns returned by the query
+and all- or part of the querydata. If the response contains a data section the columns section will always
+be available.
 
-The server reponse may contain a variable with the uri to cancel the query (partialCancelUri). The
-client may issue a DELETE request to the server using this link.
-The Presto server will retain information about finished queries for 15 minutes. When a client does
-not respond to the server (by following the nextUri links) the server will cancel these 'dead' queries
-after 5 minutes. These timeouts are hardcoded in the Presto server source code.
+The server reponse may contain a variable with the uri to cancel the query ('partialCancelUri'). The client
+may issue a DELETE request to the server using this link. Response http status-code is 204.
+
+The Presto server will retain information about finished queries for 15 minutes. When a client does not
+respond to the server (by following the 'nextUri' links) the server will cancel these 'dead' queries after
+5 minutes. These timeouts are hardcoded in the Presto server source code.
 
 ToDo
 ----
-- Make the PrestoClient class re-usable. Currently you can only start one query per instance of
-this class.
-
+- Enable PrestoClient to handle multiple running queries simultaneously. Currently you can only run one query per instance of this class.
+- Add support for https connections
 - Add support for insert/update queries (if and when Presto server supports this).
 
 Availability
@@ -80,4 +89,3 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-
--- python/prestoclient.py
+++ python/prestoclient.py
@@ -19,6 +19,7 @@
 """
 
 import httplib
+import socket
 import urllib2
 import json
 import getpass
@@ -37,7 +38,7 @@ class PrestoClient:
     Presto uses SQL as its query language. Presto is an alternative for
     Hadoop-Hive.
 
-    PrestoClient was developed using Presto 0.52 and tested on Presto 0.52 and 0.54.
+    PrestoClient was developed using Presto 0.52 and tested on Presto 0.52 and 0.54. Python version used is 2.7.6
 
     You can use this class with this sample code:
 
@@ -49,59 +50,74 @@ class PrestoClient:
     >>> presto = prestoclient.PrestoClient("localhost")
     >>>
     >>> if not presto.startquery(sql):
-    >>>     print presto.getlasterrormessage()
+    >>>     print "Error: ", presto.getlasterrormessage()
     >>> else:
     >>>     presto.waituntilfinished(True) # Remove True parameter to skip printing status messages
+    >>>
+    >>>     # We're done now, so let's show the results
     >>>     print "Columns: ", presto.getcolumns()
-    >>>     print "Datalength: ", presto.getdatalength(), " Data: ", presto.getdata()
+    >>>     if presto.getstatus() == "FAILED": print "Error : ", presto.getlasterrormessage()
+    >>>     if presto.getdata(): print "Datalength: ", presto.getnumberofdatarows(), " Data: ", presto.getdata()
 
     Presto client protocol
     ======================
 
-    The communication protocol used between Presto clients and servers is not documented. It seems to
+    The communication protocol used between Presto clients and servers is not documented yet. It seems to
     be as follows:
 
-    Client sends http POST request. Headerinformation should include: X-Presto-Catalog, X-Presto-Source,
-    X-Presto-Schema, User-Agent, X-Presto-User. The body of the request should contain the sql statement.
-    The server responds by returning JSON data. This data should contain 2 uri's. One giving the link
-    to get more information about the query execution (infoUri) and the other one giving the link to fetch
-    the next packet of data (nextUri).
+    Client sends http POST request to the Presto server, page: "/v1/statement". Header information should
+    include: X-Presto-Catalog, X-Presto-Source, X-Presto-Schema, User-Agent, X-Presto-User. The body of the
+    request should contain the sql statement. The server responds by returning JSON data (http status-code 200).
+    This reply may contain up to 3 uri's. One giving the link to get more information about the query execution
+    ('infoUri'), another giving the link to fetch the next packet of data ('nextUri') and one with the uri to
+    cancel the query ('partialCancelUri').
 
-    The client should send GET requests to the server (header: X-Presto-Source, User-Agent, X-Presto-User.
-    Body: empty) following the nextUri link from the last response
-    until the server response does not give any more nextUri links. The server response also contains a
-    'state' variable. When there is no nextUri the state should be one of: FINISHED, FAILED or CANCELED.
-    Each response by the server to a 'nextUri' may contain information about the columns returned by the
-    query and all- or part of the querydata.
+    The client should send GET requests to the server (Header: X-Presto-Source, User-Agent, X-Presto-User.
+    Body: empty) following the 'nextUri' link from the previous response until the servers response does not
+    contain an 'nextUri' link anymore. When there is no 'nextUri' the query is finished. If the last response
+    from the server included an error section ('error') the query failed, otherwise the query succeeded. If
+    the http status of the server response is anything other than 200 with Content-Type application/json, the
+    query should also be considered failed. A 503 http response means that the server is (too) busy. Retry the
+    request after waiting at least 50ms.
+    The server response may contain a 'state' variable. This is for informational purposes only (may be subject
+    to change in future implementations).
+    Each response by the server to a 'nextUri' may contain information about the columns returned by the query
+    and all- or part of the querydata. If the response contains a data section the columns section will always
+    be available.
 
-    The server reponse may contain a variable with the uri to cancel the query (partialCancelUri). The
-    client may issue a DELETE request to the server using this link.
-    The Presto server will retain information about finished queries for 15 minutes. When a client does
-    not respond to the server (by following the nextUri links) the server will cancel these 'dead' queries
-    after 5 minutes. These timeouts are hardcoded in the Presto server source code.
+    The server reponse may contain a variable with the uri to cancel the query ('partialCancelUri'). The client
+    may issue a DELETE request to the server using this link. Response http status-code is 204.
 
-    ToDo
+    The Presto server will retain information about finished queries for 15 minutes. When a client does not
+    respond to the server (by following the 'nextUri' links) the server will cancel these 'dead' queries after
+    5 minutes. These timeouts are hardcoded in the Presto server source code.
+
+    Todo
     ====
 
-        - Make the PrestoClient class re-usable. Currently you can only start one query per instance of
-        this class.
+        - Enable PrestoClient to handle multiple running queries simultaneously. Currently you can only run
+        one query per instance of this class.
+
+        - Add support for https connections
 
         - Add support for insert/update queries (if and when Presto server supports this).
 
     Availability
     ============
 
-    Source code is available through: U{https://github.com/easydatawarehousing/prestoclient}
+    PrestoClient source code is available through: U{https://github.com/easydatawarehousing/prestoclient}
 
     Additional information may be found here: U{http://www.easydatawarehousing.com/tag/presto/}
 
     """
 
-    __source = "RPresto"                        #: Client name sent to Presto server
-    __version = "0.1.0"                         #: PrestoClient version string
+    __source = "PyPrestoClient"                 #: Client name sent to Presto server
+    __version = "0.2.0"                         #: PrestoClient version string
     __useragent = __source + "/" + __version    #: Useragent name sent to Presto server
     __urltimeout = 5000                         #: Timeout in millisec to wait for Presto server to respond
     __updatewaittimemsec = 1500                 #: Wait time in millisec to wait between requests to Presto server
+    __retrywaittimemsec = 100                   #: Wait time in millisec to wait before retrying a request
+    __maximumretries = 5                        #: Maximum number of retries fro request in case of 503 errors
     __server = ""                               #: IP address or DNS name of Presto server
     __port = 0                                  #: TCP port of Presto server
     __catalog = ""                              #: Catalog name to be used by Presto server
@@ -111,9 +127,9 @@ class PrestoClient:
     __lastnexturi = ""                          #: Uri to next dataframe on the Presto server
     __lastcanceluri = ""                        #: Uri to cancel query on the Presto server
     __laststate = ""                            #: State returned by last request to Presto server
+    __clientstatus = "NONE"                     #: Status defined by PrestoClient: NONE, RUNNING, SUCCEEDED, FAILED
     __cancelquery = False                       #: Boolean, when set to True signals that query should be cancelled
-    __isstarted = False                         #: Boolean, when set to True no new queries can be started
-    __response = {}                             #: Buffer for last response of Presto server
+    __lastresponse = {}                         #: Buffer for last response of Presto server
     __columns = {}                              #: Buffer for the column information returned by the query
     __data = []                                 #: Buffer for the data returned by the query
 
@@ -147,9 +163,17 @@ class PrestoClient:
         """ Return PrestoClient version number. """
         return self.__version
 
-    def getresponse(self):
+    def getlastresponse(self):
         """ Return response of last executed request to the prestoserver. """
-        return self.__response
+        return self.__lastresponse
+
+    def getstatus(self):
+        """ Return status of the client. Note this is not the same as the state reported by the Presto server! """
+        return self.__clientstatus
+
+    def getlastserverstate(self):
+        """ Return state of the request as reported by the Presto server. """
+        return self.__laststate
 
     def getlasterrormessage(self):
         """ Return error message of last executed request to the prestoserver or empty string if there is no error. """
@@ -159,7 +183,7 @@ class PrestoClient:
         """ Return the column information of the queryresults. Nested list of datatype / fieldname. """
         return self.__columns
 
-    def getdatalength(self):
+    def getnumberofdatarows(self):
         """ Return the length of the currently buffered data in number of rows. """
         return len(self.__data)
 
@@ -168,12 +192,13 @@ class PrestoClient:
         return self.__data
 
     def cleardata(self):
-        """ Empty the data buffer. Use this function to implement your own 'streaming' data retrieval setup. """
+        """ Empty the data buffer. You can use this function to implement your own 'streaming' data retrieval setup. """
         self.__data = {}
         return
 
     def startquery(self, in_sql_statement, in_schema="default"):
-        """ Start a query. Currently, only one query per instance of the PrestoClient class is allowed.
+        """ Start a query. Currently, only one simultaneous query per instance of the PrestoClient class is allowed.
+        Starting a new query will discard any data previously retrieved !
 
         Arguments:
 
@@ -182,32 +207,44 @@ class PrestoClient:
         in_schema        -- The HDFS schema that should be used (default 'default')
 
         """
+
         headers = {"X-Presto-Catalog": self.__catalog,
                    "X-Presto-Source":  self.__source,
                    "X-Presto-Schema":  in_schema,
                    "User-Agent":       self.__useragent,
                    "X-Presto-User":    self.__user}
 
-        self.__lasterror = ""
-
+        # Prepare statement
         sql = in_sql_statement
         sql = sql.strip()
         sql = sql.rstrip(";")
 
         if sql == "":
-            self.__lasterror = "No query statement"
+            self.__lasterror = "No query entered"
             return False
 
-        if self.__isstarted:
-            self.__lasterror = "Query already started. Please create a new instance of PrestoClient class"
+        # Check current state, any status except running is okay
+        if self.__clientstatus == "RUNNING":
+            self.__lasterror = "Query already running. Please create a new instance of PrestoClient class"
             return False
 
-        self.__isstarted = True
+        # Reset variables
+        self.__lasterror = ""
+        self.__lastinfouri = ""
+        self.__lastnexturi = ""
+        self.__lastcanceluri = ""
+        self.__laststate = ""
+        self.__cancelquery = False
+        self.__lastresponse = {}
+        self.__columns = {}
+        self.__data = []
 
         try:
             conn = httplib.HTTPConnection(self.__server, self.__port, False, self.__urltimeout)
             conn.request("POST", "/v1/statement", sql, headers)
             response = conn.getresponse()
+
+            # Todo: add check on 503 return codes. In that case retry request. See __openuri
 
             if response.status != 200:
                 self.__lasterror = "Connection error: " + str(response.status) + " " + response.reason
@@ -218,11 +255,11 @@ class PrestoClient:
 
             conn.close()
 
-            self.__response = json.loads(answer)
+            self.__lastresponse = json.loads(answer)
             self.__getvarsfromresponse()
 
-        except httplib.HTTPException:
-            self.__lasterror = "Connection error: " + str(httplib.HTTPException.message)
+        except (httplib.HTTPException, socket.error) as e:
+            self.__lasterror = "Error connecting to server: " + self.__server + ":" + str(self.__port)
             return False
 
         else:
@@ -231,7 +268,8 @@ class PrestoClient:
         return True
 
     def waituntilfinished(self, in_verbose=False):
-        """ Returns when query has finished.
+        """ Returns when query has finished. Override this function to implement your own data retrieval setup.
+        For instance to run this function in a separate thread so other threads may request a cancellation.
 
         Arguments:
 
@@ -247,22 +285,27 @@ class PrestoClient:
 
             sleep(self.__updatewaittimemsec/1000)
 
+        if in_verbose:
+            print "Done: ", tries + 1, " Rows=", len(self.__data)
+
         return
 
     def queryisrunning(self):
-        """ Returns True if query is running. Use this function to implement your own data retrieval setup. """
-        if not self.__getnext():
-            return False
-
+        """ Returns True if query is running. """
         if self.__cancelquery:
             self.__cancel()
             return False
 
-        if self.__laststate == "FINISHED" or self.__laststate == "FAILED" or self.__laststate == "CANCELED":
-            # Pick up any remaining data and messages
-            while self.__getnext():
-                pass
+        if self.__lastnexturi == "":
+            # This should never happen !
+            return False
 
+        if not self.__openuri(self.__lastnexturi):
+            return False
+
+        self.__getvarsfromresponse()
+
+        if self.__lastnexturi == "":
             return False
 
         return True
@@ -278,7 +321,7 @@ class PrestoClient:
         if not self.__openuri(self.__lastinfouri):
             return {}
 
-        return self.__response
+        return self.__lastresponse
 
     def cancelquery(self):
         """ Inform Prestoclient to cancel the running query. When queryisrunning() is called
@@ -297,10 +340,24 @@ class PrestoClient:
         self.__lasterror = ""
 
         try:
-            conn = urllib2.urlopen(in_uri, None, self.__urltimeout)
-            answer = conn.read()
-            conn.close()
-            self.__response = json.loads(answer)
+            retry = True
+            retrycount = 0
+
+            while retry:
+                retrycount += 1
+                conn = urllib2.urlopen(in_uri, None, self.__urltimeout)
+                answer = conn.read()
+                conn.close()
+                self.__lastresponse = json.loads(answer)
+
+                if retrycount > self.__maximumretries:
+                    self.__lasterror = "Maximum number of retries reached"
+                    return False
+
+                if conn.getcode() == 503:
+                    sleep(self.__retrywaittimemsec/1000)
+                else:
+                    retry = False
 
         except urllib2.HTTPError as e:
             self.__lasterror = "HTTP error: " + e.reason
@@ -316,47 +373,51 @@ class PrestoClient:
         return True
 
     def __getvarsfromresponse(self):
-        """ Internal function, retrieves some information from the response of the Presto server. """
-        if "infoUri" in self.__response:
-            self.__lastinfouri = self.__response["infoUri"]
+        """ Internal function, retrieves some information from the response of the Presto server. Keep
+        the last known values, except for 'nextUri'.
 
-        if "nextUri" in self.__response:
-            self.__lastnexturi = self.__response["nextUri"]
+        """
+        if "infoUri" in self.__lastresponse:
+            self.__lastinfouri = self.__lastresponse["infoUri"]
+
+        if "nextUri" in self.__lastresponse:
+            self.__lastnexturi = self.__lastresponse["nextUri"]
         else:
             self.__lastnexturi = ""
 
-        if "partialCancelUri" in self.__response:
-            self.__lastcanceluri = self.__response["partialCancelUri"]
+        if "partialCancelUri" in self.__lastresponse:
+            self.__lastcanceluri = self.__lastresponse["partialCancelUri"]
 
-        if "state" in self.__response:
-            self.__laststate = self.__response["state"]
-        elif "stats" in self.__response:
-            if "state" in self.__response["stats"]:
-                self.__laststate = self.__response["stats"]["state"]
+        if "state" in self.__lastresponse:
+            self.__laststate = self.__lastresponse["state"]
+        elif "stats" in self.__lastresponse:
+            if "state" in self.__lastresponse["stats"]:
+                self.__laststate = self.__lastresponse["stats"]["state"]
 
         if not self.__columns:
-            if "columns" in self.__response:
-                self.__columns = self.__response["columns"]
+            if "columns" in self.__lastresponse:
+                self.__columns = self.__lastresponse["columns"]
 
-        if "data" in self.__response:
+        if "data" in self.__lastresponse:
             if self.__data:
-                self.__data.extend(self.__response["data"])
+                self.__data.extend(self.__lastresponse["data"])
             else:
-                self.__data = self.__response["data"]
+                self.__data = self.__lastresponse["data"]
+
+        # Determine state
+        if self.__lastnexturi != "":
+            self.__clientstatus = "RUNNING"
+        elif "error" in self.__lastresponse:
+            self.__clientstatus = "FAILED"
+
+            # Get errormessage
+            if "failureInfo" in self.__lastresponse["error"]:
+                if "message" in self.__lastresponse["error"]["failureInfo"]:
+                    self.__lasterror = self.__lastresponse["error"]["failureInfo"]["message"]
+        else:
+            self.__clientstatus = "SUCCEEDED"
 
         return
-
-    def __getnext(self):
-        """ Internal function, starts a new request to the Presto server using the 'nextUri' link. """
-        if self.__lastnexturi == "":
-            return False
-
-        if not self.__openuri(self.__lastnexturi):
-            return False
-
-        self.__getvarsfromresponse()
-
-        return True
 
     def __cancel(self):
         """ Internal function, sends a cancel request to the Prestoserver. """
@@ -387,5 +448,7 @@ class PrestoClient:
 
         else:
             pass
+
+        self.__clientstatus = "NONE"
 
         return True
